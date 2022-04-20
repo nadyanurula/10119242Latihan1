aa96e4e53cc6548a86ca945eb3f7ade30fec09d9
nadya nurul <nurulawaliyahnadya@gmail.com>
2022-04-20 07:18:36 +0700

    Initial commit

---
+++ app/src/main/java/com/nad/a10119242latihan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nad.a10119242latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//19 april 2020 10119242 Nadya Nurul IF6
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
